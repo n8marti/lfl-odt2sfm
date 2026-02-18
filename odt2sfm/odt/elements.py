53d596bb03939b41279ebdab00bebfbc6dae195c
Nate Marti <nate_marti@sil.org>
2026-02-18 06:37:35 -0600

    move to odfdo package

--- odt2sfm/odt/elements.py
+++ odt2sfm/odt/elements.py
@@ -1,12 +1,11 @@
 import unicodedata
 
-from odf.element import Node
-from odf.teletype import extractText
+from odfdo import Element, EText
 
 
 class OdtElement:
-    def __init__(self, element, chapter=None):
-        self.node = element
+    def __init__(self, node, chapter=None):
+        self.node = node
         self.chapter = None
         # FIXME: Normalization form should come from ODT file somehow.
         self.normalization_form = "NFC"
@@ -15,30 +14,24 @@ class OdtElement:
 
     @property
     def all_children(self):
-        return self.node.childNodes
+        return self.node.children
 
     @property
     def parent(self):
-        return self.node.parentNode
+        return self.node.parent
 
     @property
     def path(self):
         node = self.node
-        path = [node.qname[1] if node.nodeType == Node.ELEMENT_NODE else str(node)]
-        while node.parentNode is not None:
-            path.insert(0, node.parentNode.qname[1])
-            node = node.parentNode
+        path = [node.tag if isinstance(node, Element) else str(node)]
+        while node.parent is not None:
+            path.insert(0, node.parent.tag)
+            node = node.parent
         return "/".join(path)
 
     @property
     def text(self):
-        # Note: Using extractText allows special spacings (e.g. line breaks,
-        # tabs, multiple spaces) to be properly converted to unicode, but this
-        # misrepresents whether or not the SFM text has been changed from the
-        # original ODT text, since those special spacings aren't exported as
-        # editable in the SFM output.
-        # return extractText(self.node)
-        return str(self.node)
+        return self.node.text_recursive
 
     def _normalize(self, text):
         """Normalize foreign text according to current document preferences."""
@@ -51,35 +44,44 @@ class OdtElement:
 class OdtText(OdtElement):
     @property
     def data(self):
-        return self.node.data
+        return self.node
 
     @property
     def text(self):
-        return self.data
+        return self.node
 
 
 class OdtSpan(OdtElement):
     @property
     def style(self):
-        return self.node.getAttribute("stylename")
+        return self.node.get_attribute("text:style-name")
+
+    @property
+    def text(self):
+        return self.node.inner_text
 
 
 class OdtParagraph(OdtElement):
     @property
     def children(self):
-        children = []
-        for node in self.all_children:
-            if node.nodeType == Node.TEXT_NODE:
-                if len(node.data) > 0:  # ignore nodes with zero characters
-                    # print(f"|{node.data}|")
-                    children.append(node)
-            elif node.nodeType == Node.ELEMENT_NODE:
-                if node.qname[1] == "span":
-                    # from sys import stdout
+        def _filter_children(node, accumulator=None):
+            if accumulator is None:
+                accumulator = []
+            if node.text:
+                accumulator.append(node.text)
+            if node.tail:
+                accumulator.append(node.tail)
+            for child in node.children:
+                if child.tag in ("text:paragraph", "text:span"):
+                    if child.style in self.chapter.styles:
+                        accumulator.append(child)
+                else:
+                    accumulator = _filter_children(child, accumulator)
+            return accumulator
 
-                    # print(node.toXml(0, stdout))
-                    if node.getAttribute("stylename") in self.chapter.styles:
-                        children.append(node)
+        print(f"children: checking: {self.node.tag}/{self.all_children=}")
+        children = _filter_children(self.node)
+
         return children
 
     @property
@@ -88,27 +90,29 @@ class OdtParagraph(OdtElement):
             while node:
                 if node is ancestor:
                     return True
-                # NOTE: Top node has no parentNode.
-                node = node.parentNode
+                # NOTE: Top node has no parent.
+                node = node.parent
 
         spans = []
-        # FIXME: Loop through self.node.getElementsByType instead.
-        for s_node in self.chapter.all_spans:
-            # Ignore spans that are contained within other paragraphs.
-            if not descendent_of_node(s_node, self.node):
+        for c_node in self.children:
+            print(f"Checking: {c_node.tag}")
+            # Ignore non-span nodes..
+            if c_node.tag != "text:span":
+                print(f"Skipping: {c_node.tag}")
                 continue
+            print(f"Keeping: {c_node.tag}")
             # Ignore spans that contain the full paragraph's text.
-            if extractText(s_node) == self.text:
+            if c_node.text == self.text:
                 continue
             # Ignore spans whose style is not in the useful styles list.
-            if s_node.getAttribute("stylename") not in self.chapter.styles:
+            if c_node.get_attribute("text:style-name") not in self.chapter.styles:
                 continue
-            spans.append(OdtSpan(s_node))
+            spans.append(OdtSpan(c_node))
         return spans
 
     @property
     def style(self):
-        return self.node.getAttribute("stylename")
+        return self.node.get_attribute("text:style-name")
 
     # @property
     # def texts(self):
@@ -141,15 +145,15 @@ class OdtParagraph(OdtElement):
             print(f"Warning: Unmatched children for ODT ({odt_ct}) & SFM ({sfm_ct})")
             print([c.__class__.__name__ for c in self.children])
             texts = []
-            for c in self.children:
-                if c.nodeType == Node.TEXT_NODE:
-                    texts.append(c.data)
-                else:
-                    texts.append(str(c))
+            # for c in self.children:
+            #     if c.nodeType == Node.TEXT_NODE:
+            #         texts.append(c.data)
+            #     else:
+            #         texts.append(str(c))
             print(texts)
             print([c.__class__.__name__ for c in sfm_paragraph.children])
             print([c.text for c in sfm_paragraph.children])
-            print(extractText(self.node))
+            # print(extractText(self.node))
             print()
             return
 
@@ -167,9 +171,9 @@ class OdtParagraph(OdtElement):
         #         return id
         # if hasattr(node, "qname"):
         #     print(f"{node.qname[1]=}")
-        print(
-            f"{self.node.qname[1]}:{[c.qname[1] for c in self.children if hasattr(c, 'qname')]}"
-        )
+        # print(
+        #     f"{self.node.qname[1]}:{[c.qname[1] for c in self.children if hasattr(c, 'qname')]}"
+        # )
 
         idx = None
         for i, n in enumerate(self.children):
@@ -178,21 +182,21 @@ class OdtParagraph(OdtElement):
                 break
         if idx is None:
             # Node does not have updatable text.
-            print(
-                f"Skipping ignored node: {node.parentNode.qname[1]}/{node.qname[1]}: {str(node)}"
-            )
+            # print(
+            #     f"Skipping ignored node: {node.parentNode.qname[1]}/{node.qname[1]}: {str(node)}"
+            # )
             # print(f"{self.node=}; {self.children=}")
             # print(f"{[n.__class__.__name__ for n in node.childNodes]}")
             return
 
         sfm_text = self._normalize(sfm_paragraph.children[idx])
-        if node.nodeType == Node.TEXT_NODE:
-            if node.data != sfm_text:
+        if isinstance(node, EText):
+            if node != sfm_text:
                 print(
-                    f"Updating text for: {node.parentNode.getAttribute('stylename')}; from '{node.data}' to '{sfm_text}'"
+                    f"Updating text for: {node.parent.get_attribute('text:style-name')}; from '{node}' to '{sfm_text}'"
                 )
-                node.data = sfm_text
-        elif node.nodeType == Node.ELEMENT_NODE:
-            if extractText(node) != sfm_text:
-                for child_node in node.childNodes:
+                node = EText(sfm_text)
+        elif isinstance(node, Element):
+            if node.text_recursive != sfm_text:
+                for child_node in node.children:
                     self._update_node_text(child_node, sfm_paragraph)
--- tests/test_odt_submodule.py
+++ tests/test_odt_submodule.py
@@ -4,31 +4,47 @@ from pathlib import Path
 from odt2sfm.odt import OdtChapter
 from odt2sfm.odt.elements import OdtParagraph, OdtSpan
 
-# from odt2sfm.odt.elements import OdtElement, OdtParagraph, OdtSpan
+CHAPTER_PATH = Path(__file__).parent / "data" / "chapter.odt"
 
-BOOK_PATH = Path(__file__).parent / "data" / "book.odt"
+
+class TestOdtChapter(unittest.TestCase):
+    def setUp(self):
+        self.chapter = OdtChapter(CHAPTER_PATH)
+
+    def test_chapter_paragraphs(self):
+        self.assertEqual(len(self.chapter.all_paragraphs), 8)
+        self.assertEqual(
+            len(self.chapter.odt.body.paragraphs) + len(self.chapter.odt.body.headers),
+            8,
+        )
 
 
 class TestOdtElements(unittest.TestCase):
     def setUp(self):
-        self.chapter = OdtChapter(BOOK_PATH)
+        self.chapter = OdtChapter(CHAPTER_PATH)
         self.paragraph3 = OdtParagraph(
-            self.chapter.all_paragraphs[3], chapter=self.chapter
+            self.chapter.all_paragraphs[2], chapter=self.chapter
         )
         self.paragraph4 = OdtParagraph(
-            self.chapter.all_paragraphs[4], chapter=self.chapter
+            self.chapter.all_paragraphs[3], chapter=self.chapter
         )
-        self.span_normal = OdtSpan(self.chapter.all_spans[2])
+        self.span_bold = OdtSpan(self.chapter.all_spans[2])
         self.span_tabs = OdtSpan(self.chapter.all_spans[3])
+
+    def test_paragraph_spans(self):
+        self.assertEqual(len(self.chapter.all_spans), 7)
 
     def test_paragraph_text(self):
         self.assertEqual("3 3rd verse, but now 2nd paragraph.", self.paragraph4.text)
 
     def test_path(self):
-        self.assertEqual(self.paragraph3.path, "document/body/text/p")
+        self.assertEqual(
+            self.paragraph3.path,
+            "office:document-content/office:body/office:text/text:p",
+        )
 
     def test_span_text_simple(self):
-        self.assertEqual("bolded", self.span_normal.text)
+        self.assertEqual("bolded", self.span_bold.text)
 
     def test_span_text_withtabs(self):
-        self.assertEqual("boldwithtabs", self.span_tabs.text)
+        self.assertEqual("bold\twith\ttabs.", self.span_tabs.text)
